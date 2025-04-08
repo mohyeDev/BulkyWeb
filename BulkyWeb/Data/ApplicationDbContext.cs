aa2b786eae750a59c468ec22d7f1a7b2330ea884
Mohye Elsayed <Mohye20@gmail.com>
2025-04-08 06:04:40 +0200

    Seed Category

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -5,11 +5,37 @@ namespace BulkyWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
-        {
-            
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option)
+            : base(option) { }
 
         public DbSet<Categrory> Categrories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Categrory>()
+                .HasData(
+                    new Categrory
+                    {
+                        CategoryId = 1,
+                        Name = "Action",
+                        DisplayOrder = 1,
+                    },
+
+                    new Categrory
+                    {
+                        CategoryId = 2 ,
+                        Name = "SciFi",
+                        DisplayOrder = 1,
+                    },
+
+                    new Categrory
+                    {
+                        CategoryId = 3 ,
+                        Name = "History",
+                        DisplayOrder = 1,
+                    }
+                );
+        }
     }
 }
---
+++ BulkyWeb/Migrations/20250408040421_SeedCategryTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categrories",
+                columns: new[] { "CategoryId", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 1, "SciFi" },
+                    { 3, 1, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categrories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categrories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categrories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+        }
+    }
+}
