e4c089f977444044c54f60cc2f7892fcfc568f72
Mohye Elsayed <Mohye20@gmail.com>
2025-04-09 16:41:20 +0200

    Implement Category Repository

---
+++ Builky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Builky.DataAccess.Data;
+using Builky.DataAccess.Repository.IRepository;
+using Builky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builky.DataAccess.Repository
+{
+    class CategoryRepository :  Repository<Category>  , ICategoryRespoitory
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categrories.Update(category);
+        }
+    }
+}
