558543dadb84f2cd5f1cd55d54ab6ff40b4a404a
Mohye Elsayed <Mohye20@gmail.com>
2025-04-09 16:35:32 +0200

    Implement ICategoryRepository

---
+++ Builky.DataAccess/Repository/IRepository/ICategoryRespoitory.cs
@@ -0,0 +1,16 @@
+﻿using Builky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builky.DataAccess.Repository.IRepository
+{
+    public  interface ICategoryRespoitory : IRepository<Category>
+    {
+
+        void Update(Category category);
+        void save();
+    }
+}
