aee397efed3bee993ba06dd8ca793224f8b8983d
Mohye Elsayed <Mohye20@gmail.com>
2025-04-09 16:27:29 +0200

    Implement Repository Interface

--- Builky.DataAccess/Repository/IRepository/IRepository.cs
+++ Builky.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace Builky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
 
         IEnumerable<T> GetAll();
 
         T Get(Expression<Func<T,bool>> filter);
         void Add(T entity);
-        void Update(T entity);
+         
 
         void Remove(T entity);
 
---
+++ Builky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Builky.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Builky.DataAccess.Repository.IRepository;
+using Builky.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Builky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public readonly ApplicationDbContext _db;
+
+        internal DbSet<T> dbSet; 
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+      
+    }
+}
