613079da80236d490243c636bc14b6eecf16d5c1
Mohye Elsayed <Mohye20@gmail.com>
2025-04-19 13:39:41 +0200

    OrderVM and Order Controller

---
+++ Builky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,17 @@
+﻿using Builky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builky.Models.ViewModels
+{
+    public class OrderVM
+    {
+
+        public OrderHeader OrderHeader { get; set; }
+
+        public IEnumerable<OrderDetail> orderDetails { get; set; }
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Builky.DataAccess.Repository.IRepository;
+using Builky.Models.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _unitOfWork.orderHeaderRespoitory.GetAll(includeProperties: "ApplicationUserId").ToList();
+            return Json(new { data = orderHeaders });
+
+        }
+        #endregion
+
+
+
+    }
+}
